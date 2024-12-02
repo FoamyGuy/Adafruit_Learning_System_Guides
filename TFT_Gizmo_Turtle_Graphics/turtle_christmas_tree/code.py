25e93a86f745866eb48151b62ab6a87d222c7ac7
foamyguy <foamyguy@gmail.com>
2024-12-02 17:17:07 -0600

    use turtle mode standard on examples that need it

--- TFT_Gizmo_Turtle_Graphics/turtle_christmas_tree/code.py
+++ TFT_Gizmo_Turtle_Graphics/turtle_christmas_tree/code.py
@@ -15,6 +15,7 @@ display_bus = displayio.FourWire(spi, command=board.TX, chip_select=board.RX)
 display = ST7789(display_bus, width=240, height=240, rowstart=80,
                  backlight_pin=board.A3, rotation=180)
 turtle = turtle(display)
+turtle.mode("standard")
 #==| Turtle Gizmo Setup end |=========================================
 
 # Fractal Christmas Tree:
